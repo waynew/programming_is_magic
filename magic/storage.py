88deacdb93ce7ef6cde72fc999e1eb23acde44b1
Wayne Werner <wayne@waynewerner.com>
2014-03-29 19:07:43 -0500

    Several tests creating users

--- magic/storage.py
+++ magic/storage.py
@@ -1,36 +1,59 @@
+import hashlib
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import IntegrityError
+
+salt = 'sodium chloride'
 
 engine = create_engine('sqlite:///:memory:')
 Base = declarative_base()
 
-class Posty(Base):
-    __tablename__ = "posts"
+class User(Base):
+    __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    data = Column(String)
+    username = Column(String, primary_key=True)
+    hashword = Column(String)
 
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, username, hashword):
+        self.username = username
+        self.hashword = hashword
 
 
     def __repr__(self):
-        return "<Posty %s>" % self.data
-
-
-Base.metadata.create_all(engine)
+        return "<User %s>" % self.username
 
 Session = sessionmaker(bind=engine)
 
 
-def store_post(data):
-    session = Session()
-    p = Posty(data)
-    session.add(p)
-    session.commit()
+def init_db():
+    Base.metadata.create_all(engine)
 
 
-def list_posts():
+def _make_hashword(password):
+    sha = hashlib.sha256()
+    sha.update(password.encode())
+    sha.update(salt.encode())
+    return sha.hexdigest()
+
+
+def create_user(username, password):
+    '''Create a user with the given username and password.
+    Hash and salt the passwords. Mmmm, delicious salt.
+
+    '''
     session = Session()
-    return session.query(Posty).all()
+    session.add(User(username, _make_hashword(password)))
+    try:
+        session.commit()
+    except IntegrityError:
+        raise ValueError("Username '%s' already exists" % username)
+
+
+def get_user(username):
+    '''Return the user that corresponds to the given username.
+    If no user exists with that name, return None.
+
+    '''
+    session = Session()
+    return session.query(User).filter_by(username=username).first()
--- magic/tests/test_storage.py
+++ magic/tests/test_storage.py
@@ -1,4 +1,67 @@
-import storage
+import hashlib
+import pytest
+from .. import storage
 
-def test_fail():
-    assert False
+def setup_function(function):
+    storage.init_db()
+
+
+def teardown_function(function):
+    storage.Base.metadata.drop_all(storage.engine)
+
+
+@pytest.mark.parametrize('username', [
+    'roscivs',
+    'bottia',
+    'srilyk',
+])
+def test_after_create_user_get_user_should_return_user_with_username(username):
+    expected_username = username
+
+    storage.create_user(username=expected_username, password="Whatever")
+    user = storage.get_user(username=expected_username)
+
+    assert user.username == expected_username
+
+
+def test_make_hashword_should_return_same_value_with_repeated_input():
+    hash_input = "fnordy"
+
+    output_one = storage._make_hashword(hash_input)
+    output_two = storage._make_hashword(hash_input)
+
+    assert output_one == output_two
+
+
+def test_make_hashword_should_return_sha1_updated_by_pw_and_salt():
+    password = "Password123"
+    salt = "Sodium Chloride"
+    storage.salt = salt
+    sha = hashlib.sha256()
+    sha.update(password.encode())
+    sha.update(salt.encode())
+
+    assert storage._make_hashword(password) == sha.hexdigest()
+
+
+def test_returned_user_should_have_correct_hashword():
+    expected_username = "fnord"
+    password = "something you can't see"
+    expected_hashword = storage._make_hashword(password)
+    storage.create_user(expected_username, password)
+
+    user = storage.get_user(expected_username)
+
+    assert user.username == expected_username
+    assert user.hashword == expected_hashword
+
+
+def test_get_user_for_username_not_added_should_return_None():
+    assert storage.get_user('roscivs') == None
+
+
+def test_create_user_twice_should_throw_ValueError():
+    username = 'Jim and I'
+    storage.create_user(username, 'whatev')
+    with pytest.raises(ValueError):
+        storage.create_user(username, 'something different')
